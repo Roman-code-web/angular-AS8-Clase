c0ca40ddc50d05e081359863f265c11be2767a51
Roman-code-web <selenerg.275@gmail.com>
2022-12-30 10:01:59 -0500

    envio solicitado termino de directiva ngif mostrando los div si el login es valido

--- src/app/login/formulario/formulario.component.ts
+++ src/app/login/formulario/formulario.component.ts
@@ -9,15 +9,20 @@ export class FormularioComponent {
   usuario:string="Angular";
   password:string="123456";
   verificacion!:boolean;
+  validacion!:boolean;
   login(user:string,pass:string){
+    console.log(this.verificacion)
     if(this.usuario==user && this.password==pass ){
       this.verificacion=true;
+      this.validacion=true;
       console.log(this.verificacion);
   }else if(this.usuario!=user && this.password!=pass){
       this.verificacion=false;
+      this.validacion=false;
       console.log(this.verificacion);
   }else{
       this.verificacion=false;
+      this.validacion=false;
       console.log(this.verificacion);
   }
 }
